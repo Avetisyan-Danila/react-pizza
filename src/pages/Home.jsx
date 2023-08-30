e255a59568e91c90d43ccc42bd0cab5d1689a20e
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-08-30 23:10:43 +0300

    router + css modules

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+import PizzaBlock from "../components/PizzaBlock";
+import { useEffect, useState } from "react";
+
+function Home() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://64e6699c09e64530d17ff99d.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock {...obj} key={obj.id} />)}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
