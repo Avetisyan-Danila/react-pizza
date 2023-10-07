e65e0e0609fc1bdb1181208f8879fa59963cc155
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-10-07 14:06:49 +0300

    code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,24 @@ import "./scss/app.scss";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ "./pages/Cart"));
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<div>Идёт загрузка корзины . . .</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </div>
     </div>
   );
